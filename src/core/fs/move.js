d0c557c70ba9ccb5fb72404b909ae25d3c6d510f
The-click <ramzanmal04@mail.ru>
2025-05-04 02:16:30 +0300

    refactor: add class move

--- src/core/fs/move.js
+++ src/core/fs/move.js
@@ -1,14 +1,50 @@
-import errrHandler from "../../utils/errorHandler.js";
-import copyFileCore from "./copy.js";
-import removeFileCore from "./remove.js";
+import { pipeline } from "node:stream/promises";
+import fs from "node:fs/promises";
+import { createReadStream, createWriteStream } from "fs";
+import path from "path";
+import FsCommandBase from "./base.js";
 
-const moveFileCore = async (oldFilePath, dirPath) => {
-    try {
-        await copyFileCore(oldFilePath, dirPath);
-        await removeFileCore(oldFilePath);
-    } catch (e) {
-        errrHandler(e);
+class MoveCLICommand extends FsCommandBase {
+    #name = "mv";
+
+    get name() {
+        return this.#name;
     }
-};
 
-export default moveFileCore;
+    async execute(args) {
+        try {
+            const { oldFilePath, newFilePath } = await this.getArgs(args);
+            const readStream = createReadStream(oldFilePath);
+            const writeStream = createWriteStream(newFilePath);
+            await pipeline(readStream, writeStream);
+            await fs.rm(oldFilePath);
+        } catch (e) {
+            this.errorHandler(e);
+        }
+    }
+
+    async getArgs(args) {
+        const [oldFilePath, dirPath] = this.validatePassedArgs(args, 2);
+        const [absOldFilePath, absDirPath] = this.getAbsolutePath([
+            oldFilePath,
+            dirPath,
+        ]);
+        const newFilePath = path.resolve(
+            absDirPath,
+            path.win32.basename(oldFilePath)
+        );
+
+        if (
+            (await this.isFileExist(newFilePath)) ||
+            !(await this.isFileExist(oldFilePath))
+        ) {
+            const error = new Error("Invalid input");
+            error.code = "FEXIST";
+            throw error;
+        }
+
+        return { oldFilePath: absOldFilePath, newFilePath };
+    }
+}
+
+export default MoveCLICommand;
