d78801eeb4de3acfa8b4c94000e0b898e114e9f2
The-click <ramzanmal04@mail.ru>
2025-05-04 00:23:37 +0300

    refactor: add class for copy and create

--- src/core/fs/copy.js
+++ src/core/fs/copy.js
@@ -1,28 +1,47 @@
 import { pipeline } from "node:stream/promises";
 import fs from "fs";
 import path from "path";
-import errrHandler from "../../utils/errorHandler.js";
-import isFileExist from "./isFileExist.js";
+import FsCommandBase from "./base.js";
 
-const copyFileCore = async (oldFilePath, dirPath) => {
-    try {
-        const readStream = fs.createReadStream(oldFilePath);
-        const newFilePath = path.resolve(
-            dirPath,
-            path.win32.basename(oldFilePath)
-        );
-        const writeStream = fs.createWriteStream(newFilePath);
+class CopyCLICommand extends FsCommandBase {
+    #name = "copy";
 
-        if (await isFileExist(newFilePath)) {
-            const error = new Error("File already exists");
-            error.code = "FEXIST";
-            throw error;
-        }
-
-        await pipeline(readStream, writeStream);
-    } catch (e) {
-        errrHandler(e);
+    get name() {
+        return this.#name;
     }
-};
 
-export default copyFileCore;
+    async execute(args) {
+        try {
+            const { oldFilePath, dirPath } = this.getArgs(args);
+            const readStream = fs.createReadStream(oldFilePath);
+            const newFilePath = path.resolve(
+                dirPath,
+                path.win32.basename(oldFilePath)
+            );
+            const writeStream = fs.createWriteStream(newFilePath);
+
+            if (await this.isFileExist(newFilePath)) {
+                const error = new Error("File already exists");
+                error.code = "FEXIST";
+                throw error;
+            }
+
+            await pipeline(readStream, writeStream);
+        } catch (e) {
+            this.errorHandler(e);
+        }
+    }
+
+    getArgs(args) {
+        const [oldFilePath, dirPath] = this.validatePassedArgs(args, 2);
+
+        const [absOldFilePath, absDirPath] = this.getAbsolutePath([
+            oldFilePath,
+            dirPath,
+        ]);
+
+        return { oldFilePath: absOldFilePath, dirPath: absDirPath };
+    }
+}
+
+export default CopyCLICommand;
--- src/core/fs/create.js
+++ src/core/fs/create.js
@@ -1,20 +1,55 @@
 import fs from "node:fs/promises";
-import errrHandler from "../../utils/errorHandler.js";
+import FsCommandBase from "./base.js";
 
-const createCore = async (createPath, type) => {
-    try {
-        if (type === "file") {
+class AddCLICommand extends FsCommandBase {
+    #name = "add";
+
+    get name() {
+        return this.#name;
+    }
+
+    async execute(args) {
+        try {
+            const { createPath } = this.getArgs(args);
+
             await fs.writeFile(createPath, "", {
                 flag: "wx",
             });
+        } catch (e) {
+            this.errorHandler(e);
         }
-
-        if (type === "dir") {
-            await fs.mkdir(createPath, { recursive: false });
-        }
-    } catch (err) {
-        errrHandler(err);
     }
-};
 
-export default createCore;
+    getArgs(args) {
+        const [newFileName] = this.validatePassedArgs(args, 1);
+        const [createPath] = this.getAbsolutePath([newFileName]);
+
+        return { createPath };
+    }
+}
+class MkDirCLICommand extends FsCommandBase {
+    #name = "mkdir";
+
+    get name() {
+        return this.#name;
+    }
+
+    async execute(args) {
+        try {
+            const { createPath } = this.getArgs(args);
+
+            await fs.mkdir(createPath, { recursive: false });
+        } catch (e) {
+            this.errorHandler(e);
+        }
+    }
+
+    getArgs(args) {
+        const [newDirName] = this.validatePassedArgs(args, 1);
+        const [createPath] = this.getAbsolutePath([newDirName]);
+
+        return { createPath };
+    }
+}
+
+export { AddCLICommand, MkDirCLICommand };
