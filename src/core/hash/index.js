c27aea1f9c5d83f437cbd9ccf4ff3a464a01eb64
The-click <ramzanmal04@mail.ru>
2025-05-04 12:15:29 +0300

    refactor: add hash class

--- src/core/hash/index.js
+++ src/core/hash/index.js
@@ -1,23 +1,40 @@
 import fs from "fs";
 import crypto from "crypto";
-import errrHandler from "../../utils/errorHandler.js";
+import CommandBase from "../cli-command/CommandBase.js";
 
-const calculateHash = async (pathToFile) => {
-    try {
-        const hashValue = await getHash(pathToFile);
-        console.log(hashValue);
-    } catch (error) {
-        errrHandler(error);
+class HashCLICommand extends CommandBase {
+    #name = "hash";
+
+    get name() {
+        return this.#name;
     }
-};
 
-const getHash = (path) =>
-    new Promise((resolve, reject) => {
-        const hash = crypto.createHash("sha256");
-        const rs = fs.createReadStream(path);
-        rs.on("error", reject);
-        rs.on("data", (chunk) => hash.update(chunk));
-        rs.on("end", () => resolve(hash.digest("hex")));
-    });
+    async execute(args) {
+        try {
+            const { pathToFile } = this.getArgs(args);
+            const hashValue = await this.getHash(pathToFile);
+            this.printReuslt(hashValue);
+        } catch (e) {
+            this.errorHandler(e);
+        }
+    }
 
-export default calculateHash;
+    getHash(path) {
+        return new Promise((resolve, reject) => {
+            const hash = crypto.createHash("sha256");
+            const rs = fs.createReadStream(path);
+            rs.on("error", reject);
+            rs.on("data", (chunk) => hash.update(chunk));
+            rs.on("end", () => resolve(hash.digest("hex")));
+        });
+    }
+
+    getArgs(args) {
+        const [pathToFile] = this.validatePassedArgs(args, 1);
+        const [absPathToFile] = this.getAbsolutePath([pathToFile]);
+
+        return { pathToFile: absPathToFile };
+    }
+}
+
+export default HashCLICommand;
